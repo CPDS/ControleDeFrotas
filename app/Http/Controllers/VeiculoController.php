d172eb121ac6b0c1608b196c93f2721503a49200
camiloxlr <camilomoreira81@gmail.com>
2018-07-25 11:05:35 -0300

    agrvai

--- app/Http/Controllers/VeiculoController.php
+++ app/Http/Controllers/VeiculoController.php
@@ -29,11 +29,42 @@ class VeiculoController extends Controller
 
     private function setBtns(Veiculo $veiculos){
     	$dados = "data-id='$veiculos->id' data-nome='$veiculos->nome' data-placa='$veiculos->placa' data-tipo_combustivel='$veiculos->tipo_combustivel' ";
-      $dadosVisualizar = "data-nome='$veiculos->nome' data-placa='$veiculos->placa 'data-tipo_combustivel='$veiculos->tipo_combustivel' ";
+        $dadosVisualizar = "data-nome='$veiculos->nome' data-placa='$veiculos->placa 'data-tipo_combustivel='$veiculos->tipo_combustivel' ";
     	$btnVer= "<a class='btn btn-primary btn-sm btnVer' title='Ver Veículo' $dados ><i class='fa fa-eye'></i></a> ";
     	$btnEditar= "<a class='btn btn-warning btn-sm btnEditar' title='Editar Veículo' $dados><i class ='fa fa-pencil'></i></a> ";
     	$btnDeletar= "<a class='btn btn-danger btn-sm btnDeletar' title='Deletar Veículo' data-id='$veiculos->id'><i class='fa fa-trash'></i></a>";
 
     	return $btnVer.$btnEditar.$btnDeletar;
+    }
+    
+    public function store(Request $request) {
+        $rules = array(
+              'nome' => 'required',
+              'placa' => 'required',
+              'tipo_combustivel' => 'required'
+        );
+        $attributeNames = array(
+            'nome' => 'Nome',
+            'placa' => 'Placa',
+            'tipo_combustivel' => 'Tipo de Combustível'
+            
+        );
+        $messages = array(
+            'same' => 'Essas senhas não coincidem.'
+        );
+        $validator = Validator::make(Input::all(), $rules, $messages);
+        $validator->setAttributeNames($attributeNames);
+        if ($validator->fails()){
+                return Response::json(array('errors' => $validator->getMessageBag()->toArray()));
+        }else {
+            $Veiculo = new Veiculo();
+            $Veiculo->nome = $request->nome;
+            $Veiculo->placa = $request->placa;
+            $Veiculo->tipo_combustivel = $request->tipo_combustivel;
+            $Veiculo->save();
+            //$Veiculo->setAttribute('titulo', $Veiculo->titulo);
+            //$Veiculo->setAttribute('descricao', $Veiculo->descricao);
+            return response()->json($Veiculo);
+        }
     }
 }
