6d0cf0ddecc91a8ee4dd54ca72dbaf678a91baac
corvolino <corvolino@protonmail.com>
2018-11-13 16:31:43 +0000

    oi

--- resources/views/percurso/modals/criar_editar_percurso.blade.php
+++ resources/views/percurso/modals/criar_editar_percurso.blade.php
@@ -17,18 +17,18 @@
           <div class="row" style="width: 100%">
 
             <div class="form-group col-md-6">
-              <strong>Data de Saída:</strong>
+              <strong>Hora de Saída:</strong>
               <div class="input-group">
                 <span class="input-group-addon"><i class="fa fa-clock-o"></i></span>
-                <input maxlength="250" id="hora_saida" class="form-control" name="hora_saida" type="date">
+                <input maxlength="250" id="hora_saida" class="form-control" name="hora_saida" type="time">
               </div>
             </div>
 
             <div class="form-group col-md-6">
-              <strong>Data de Chegada:</strong>
+              <strong>Hora de Chegada:</strong>
               <div class="input-group">
                 <span class="input-group-addon"><i class="fa fa-clock-o"></i></span>
-                <input id="hora_chegada" maxlength="250" class="form-control" name="hora_chegada" type="date">
+                <input id="hora_chegada" maxlength="250" class="form-control" name="hora_chegada" type="time">
               </div>
             </div>
 
