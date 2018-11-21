6381a5522b916a4fcd5ba644b430dafb3d7edce3
camiloxlr <camilomoreira81@gmail.com>
2018-11-21 11:47:19 -0300

    upgrades

--- resources/views/vendor/adminlte/layouts/partials/footer.blade.php
+++ resources/views/vendor/adminlte/layouts/partials/footer.blade.php
@@ -2,8 +2,7 @@
 <footer class="main-footer">
     <!-- To the right -->
     <div class="pull-right hidden-xs">
-        <a href="https://github.com/acacha/adminlte-laravel"></a><b>admin-lte-laravel</b></a>. {{ trans('adminlte_lang::message.descriptionpackage') }}
     </div>
     <!-- Default to the left -->
-    <strong>Copyright &copy; 2017 <a href="http://acacha.org">Acacha.org</a>.</strong> {{ trans('adminlte_lang::message.createdby') }} <a href="http://acacha.org/sergitur">Sergi Tur Badenas</a>. {{ trans('adminlte_lang::message.seecode') }} <a href="https://github.com/acacha/adminlte-laravel">Github</a>
+    <strong>Copyright &copy; 2018 <a href="http://frotas.cpds.uesb.br">Controle de Frotas</a>
 </footer>
